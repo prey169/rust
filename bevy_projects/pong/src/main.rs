80d7d666e786bcc38e716424885b99df8b16fb65
francisdrossi <francisdrossi@gmail.com>
2024-04-28 10:24:04 -0400

    adding new pong proj

---
+++ bevy_projects/pong/src/main.rs
@@ -0,0 +1,229 @@
+use bevy::{
+    math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume},
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
+
+const BALL_SIZE: f32 = 5.;
+const PADDLE_SPEED: f32 = 1.;
+const PADDLE_WIDTH: f32 = 10.;
+const PADDLE_HEIGHT: f32 = 50.;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Ai;
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Collision {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Component)]
+struct PaddleBundle {
+    paddle: Paddle,
+    shape: Shape,
+    position: Position,
+    velocity: Velocity,
+}
+
+impl PaddleBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            paddle: Paddle,
+            shape: Shape(Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
+            position: Position(Vec2::new(x, y)),
+            velocity: Velocity(Vec2::new(0., 0.)),
+        }
+    }
+}
+
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Shape(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    shape: Shape,
+    position: Position,
+    velocity: Velocity,
+}
+
+impl BallBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            ball: Ball,
+            shape: Shape(Vec2::new(BALL_SIZE, BALL_SIZE)),
+            position: Position(Vec2::new(0., 0.)),
+            velocity: Velocity(Vec2::new(x, y)),
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_ball, spawn_camera, spawn_paddles))
+        .add_systems(
+            Update,
+            (
+                move_ball,
+                // Add our projection system to run
+                // after we move the ball so we are
+                // not reading movement one frame behind
+                project_positions.after(move_ball),
+                handle_collisions.after(move_ball),
+            ),
+        )
+        .run();
+}
+
+fn spawn_paddles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Query<&Window>,
+) {
+    println!("Spawning paddles...");
+
+    if let Ok(window) = window.get_single() {
+        let window_width = window.resolution.width();
+        let padding = 50.;
+        let right_paddle_x = window_width / 2. - padding;
+        let left_paddle_x = -window_width / 2. + padding;
+
+        let mesh = Mesh::from(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
+        let material = ColorMaterial::from(Color::rgb(0., 1., 0.));
+
+        let mesh_handle = meshes.add(mesh);
+        //let material_handle = materials.add(material);
+
+        commands.spawn((
+            Player,
+            PaddleBundle::new(right_paddle_x, 0.),
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.clone().into(),
+                material: materials.add(ColorMaterial::from(Color::rgb(0., 1., 0.))),
+                ..default()
+            },
+        ));
+
+        commands.spawn((
+            Ai,
+            PaddleBundle::new(left_paddle_x, 0.),
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.into(),
+                material: materials.add(ColorMaterial::from(Color::rgb(0., 0., 1.))),
+                ..default()
+            },
+        ));
+    }
+}
+
+fn spawn_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    println!("Spawning ball...");
+
+    let shape = Mesh::from(Circle::new(BALL_SIZE));
+    let color = ColorMaterial::from(Color::rgb(1., 0., 0.));
+
+    // `Assets::add` will load these into memory and return a
+    // `Handle` (an ID) to these assets. When all references
+    // to this `Handle` are cleaned up the asset is cleaned up.
+
+    let mesh_handle = meshes.add(shape);
+    let material_handle = materials.add(color);
+
+    commands.spawn((
+        BallBundle::new(1., 0.),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
+            ..Default::default()
+        },
+    ));
+}
+
+//Returns `Some` if `ball` collides with `wall`
+// The returned `Collision` is the side of `wall`
+// that the `ball` hit.
+fn collides_with_side(ball: BoundingCircle, wall: Aabb2d) -> Option<Collision> {
+    if !ball.intersects(&wall) {
+        return None;
+    }
+    let closest_point = wall.closest_point(ball.center());
+    let offset = ball.center() - closest_point;
+
+    let side = if offset.x.abs() > offset.y.abs() {
+        if offset.x < 0. {
+            Collision::Left
+        } else {
+            Collision::Right
+        }
+    } else if offset.y > 0. {
+        Collision::Top
+    } else {
+        Collision::Bottom
+    };
+
+    Some(side)
+}
+
+fn handle_collisions(
+    mut ball: Query<(&mut Velocity, &Position, &Shape), With<Ball>>,
+    other_things: Query<(&Position, &Shape), Without<Ball>>,
+) {
+    if let Ok((mut ball_velocity, ball_position, ball_shape)) = ball.get_single_mut() {
+        for (position, shape) in &other_things {
+            if let Some(collision) = collides_with_side(
+                BoundingCircle::new(ball_position.0, ball_shape.0.x),
+                Aabb2d::new(position.0, shape.0 / 2.),
+            ) {
+                match collision {
+                    Collision::Left | Collision::Right => ball_velocity.0.x *= -1.,
+                    Collision::Top | Collision::Bottom => ball_velocity.0.y *= -1.,
+                }
+            }
+        }
+    }
+}
+
+fn move_ball(
+    // Give me all the positions that also contain a `Ball` component
+    mut ball: Query<(&mut Position, &Velocity), With<Ball>>,
+) {
+    if let Ok((mut position, velocity)) = ball.get_single_mut() {
+        position.0 += velocity.0
+    }
+}
+
+fn project_positions(mut ball: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut ball {
+        // Here we are iterating over the query to get the
+        // components from our game world
+        transform.translation = position.0.extend(0.);
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty().insert(Camera2dBundle::default());
+}
